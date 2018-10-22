b6fe25a8118c91bdf3e738d9130cc684ab40cdf9
heenasatyarthi02 <44374209+heenasatyarthi02@users.noreply.github.com>
2018-10-23 00:12:06 +0530

    Updated FlightBookingTest.java

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -1,10 +1,10 @@
-import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import Base.WebDriverLibrary;
+import Screens.FlightBooking;
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,78 +12,45 @@ import java.util.List;
 
 public class FlightBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+    private WebDriver webDriver;
+    private WebDriverLibrary webDriverLibrary;
+    private FlightBooking flightBooking;
 
+    @Before
+    public void setUp() {
+        webDriver = new ChromeDriver();
+        webDriverLibrary = new WebDriverLibrary(webDriver);
+        webDriverLibrary.get("https://www.cleartrip.com");
+        flightBooking = new FlightBooking();
+    }
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
-
-        setDriverPath();
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
-        driver.findElement(By.id("OneWay")).click();
-
-        driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
-
+        webDriverLibrary.waitFor(2000);
+        webDriverLibrary.findElementAndClick(flightBooking.getOneWay());
+        webDriverLibrary.findElementAndClear(flightBooking.getFromTag());
+        webDriverLibrary.findElementAndEnterText(flightBooking.getFromTag(), "Bangalore");
         //wait for the auto complete options to appear for the origin
-
-        waitFor(2000);
-        List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+        webDriverLibrary.waitFor(2000);
+        List<WebElement> originOptions = webDriverLibrary.findElementsInAListByTagName(flightBooking.getUi_id_1(), flightBooking.getLi());
         originOptions.get(0).click();
-
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
-
+        webDriverLibrary.findElementAndClear(flightBooking.getToTag());
+        webDriverLibrary.findElementAndEnterText(flightBooking.getToTag(), "Delhi");
         //wait for the auto complete options to appear for the destination
-
-        waitFor(2000);
+        webDriverLibrary.waitFor(2000);
         //select the first item from the destination auto complete list
-        List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
+        List<WebElement> destinationOptions = webDriverLibrary.findElementsInAListByTagName(flightBooking.getUi_id_2(), flightBooking.getLi());
         destinationOptions.get(0).click();
-
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
-
+        webDriverLibrary.findElementAndClick(flightBooking.getDatePicker());
         //all fields filled in. Now click on search
-        driver.findElement(By.id("SearchBtn")).click();
-
-        waitFor(5000);
+        webDriverLibrary.findElementAndClick(flightBooking.getSearchBtn());
+        webDriverLibrary.waitFor(5000);
         //verify that result appears for the provided journey search
-        Assert.assertTrue(isElementPresent(By.className("searchSummary")));
-
-        //close the browser
-        driver.quit();
-
+        Assert.assertTrue(webDriverLibrary.isElementPresentOnScreen(flightBooking.getSearchSummary()));
     }
 
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
+    @After
+    public void tearDown(){
+        webDriver.quit();
     }
 }
